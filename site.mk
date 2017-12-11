2622b2579e1b98293c9984e3312c158ccae44694
Martin Geno <geno+dev@fireorbit.de>
2017-12-11 22:09:18 +0100

    gluon master with current lede - ac1200pro

--- site.mk
+++ site.mk
@@ -12,14 +12,12 @@ GLUON_SITE_PACKAGES := \
 	gluon-ebtables-filter-multicast \
 	gluon-ebtables-filter-ra-dhcp \
 	gluon-ebtables-segment-mld \
-	gluon-luci-admin \
-	gluon-luci-autoupdater \
-	gluon-luci-mesh-vpn-fastd \
-	gluon-luci-portconfig \
-	gluon-luci-private-wifi \
-	gluon-luci-wifi-config \
+	gluon-web-admin \
+	gluon-web-autoupdater \
+	gluon-web-mesh-vpn-fastd \
+	gluon-web-private-wifi \
+	gluon-web-wifi-config \
 	gluon-mesh-vpn-fastd \
-	gluon-next-node \
 	gluon-radv-filterd \
 	gluon-radvd \
 	gluon-setup-mode \
